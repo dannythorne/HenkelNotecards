b49f2b4271e25ce0e80803b4e94cf65f03381039
Danny Thorne <dannythorne@gmail.com>
2014-12-11 12:07:08 -0500

    Keywords and Work in Notecard dropdown

--- Cites/notecards_read.php
+++ Cites/notecards_read.php
@@ -1,6 +1,8 @@
 <?php
 require_once("../global_vars.php");
 
+$id = "Id"; // ...pending change to "id".
+
 $mysqli = new mysqli( $host, $username, $password, $database);
 
 if( mysqli_connect_errno())
@@ -25,10 +27,34 @@ else
     $numrows = $result->num_rows;
     $numcols = $result->field_count;
 
-    while( $row = $result->fetch_row())
+    while( $row = $result->fetch_assoc())
     {
-      echo "<option>";
-      echo $row[0];
+      $getkeyword = "";
+      $getkeyword = $getkeyword."select * from Keyword";
+      $getkeyword = $getkeyword."            , KeywordNotecard";
+      $getkeyword = $getkeyword."            , Notecard";
+      $getkeyword = $getkeyword."        where ";
+      $getkeyword = $getkeyword."Keyword.id=KeywordNotecard.KeywordId";
+      $getkeyword = $getkeyword." and ";
+      $getkeyword = $getkeyword."Notecard.id=KeywordNotecard.NotecardId";
+      $getkeyword = $getkeyword." and ";
+      $getkeyword = $getkeyword."Notecard.id=".$row[$id];
+      $getkeyword = $getkeyword.";";
+
+      $keyword = $mysqli->query($getkeyword);
+      $keywords = "";
+      while( $keywordrow = $keyword->fetch_assoc())
+      {
+        $keywords = $keywords." \"".$keywordrow["Keyword"]."\"";
+      }
+
+      $getwork = "select * from Work where id=".$row['workId'].";";
+      $work = $mysqli->query($getwork);
+      $workrow = $work->fetch_assoc();
+      $workname = $workrow["Name"];
+
+      echo "<option value='".$row[$id]."'>";
+      echo $keywords." in ".$workname;
       echo "</option>";
     }
   }
