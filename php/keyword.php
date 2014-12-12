e007e721686c1269d1e08d588e000ab29950cfbe
Danny Thorne <dannythorne@gmail.com>
2014-12-11 19:00:00 -0500

    initial list

--- php/keyword.php
+++ php/keyword.php
@@ -1,5 +1,40 @@
 <?php
+require_once("../global_vars.php");
+
 $id = $_GET["id"];
 $keyword = $_GET["keyword"];
 echo 'Notecard for "'.$keyword.' (id='.$id.')". (TODO)';
+
+$mysqli = new mysqli( $host, $username, $password, $database);
+
+if( mysqli_connect_errno())
+{
+  echo "Error connecting.";
+}
+else
+{
+  $query = "";
+  $query = $query."select * from Keyword, KeywordNotecard, Notecard";
+  $query = $query." where Keyword.id=KeywordNotecard.KeywordId";
+  $query = $query." and Notecard.id=KeywordNotecard.NotecardId";
+  $query = $query." and Keyword.id=".$id;
+  $query = $query.";";
+  //echo $query;
+
+  $result = $mysqli->query($query);
+  if( $result)
+  {
+    while( $row = $result->fetch_assoc())
+    {
+      echo " Notecard ".$row['NotecardId']." ";
+    }
+  }
+  else
+  {
+    echo "ERROR";
+  }
+
+  $mysqli->close();
+}
+
 ?>
