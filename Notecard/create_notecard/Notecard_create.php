89ecefc1acdc7c81ed026da0de862fb8038873e0
Sara3 <sabera.daqiq@yahoo.com>
2014-12-13 14:00:28 -0500

    CRUD

---
+++ Notecard/create_notecard/Notecard_create.php
@@ -0,0 +1,59 @@
+﻿<!DOCTYPE html>
+<html>
+<head>
+<link href="../../css/common.css" rel="stylesheet" type="text/css" />
+<title>Notecard Create</title>
+</head>
+<body>
+<p id="nav">
+<span class="navitem"><a href="../../index.html">Index</a></span>
+|
+<span class="navitem"><a href="../index.html">Notecard</a></span>
+</p>
+<h1>Henkel's NoteCards</h1>
+<h2>Notecard Table</h2>
+<h3>Create</h3>
+
+<?php
+require_once("../../global_vars.php");
+$mysqli = new mysqli( $host, $username, $password, $database );
+if( mysqli_connect_errno() )
+{
+  echo "Connection error.";
+}
+else
+{
+	$Notecard = $_GET["Notecard"];
+	if( $Notecard === "" )
+	{
+		echo "Can't add empty Notecard name.";
+		echo "<p id='nav'>";
+		echo "<span class='navitem'><a href='./'>Add a Notecard</a></span>";
+		echo "</p>";
+	}
+	else
+	{
+		$query = $query."insert into Notecard values(0,";
+		$query = $query."'".$Notecard."');";
+		$result = $mysqli->query($query);
+		if($result)
+		{
+			echo "$Notecard "; 
+			echo "added.";
+			echo "<p id='nav'>";
+			echo "<span class='navitem'><a href='./'>Add Notecard Notecard</a></span>";
+		}
+		else
+		{
+			echo "Error";
+			echo "<span class='navitem'><a href='./'>Add an Notecard</a></span>";
+		}
+	}
+}
+$mysqli->close();
+$mysqli->free();
+?>
+
+
+</body>
+</html>
---
+++ Notecard/read_notecard/Notecard_read.php
@@ -0,0 +1,66 @@
+﻿!DOCTYPE HTML>
+<html>
+<head>
+<link href="../../css/common.css" rel="stylesheet" type="text/css"/>
+<title>Notecard Read</title>
+</head>
+<body>
+
+<p id="nav">
+<span class="navitem"><a href="../../">Index</a></span>
+|
+<span class="navitem"><a href="../">Notecard</a></span>
+</p>
+
+<h1>Henkel's NoteCards</h1>
+<h2>Notecard Table</h2> 
+<h3>Read</h3> 
+
+<?PHP
+	require_once("../../global_vars.php");
+	$mysqli = new mysqli($host, $username, $password, $database);
+	if( mysqli_connect_errno() )
+	{
+		echo "PHP ERROR: ".mysqli_connect_errno();
+	}
+	else
+	{
+		$query = "select Name from Notecard;";
+		$result = $mysqli->query($query);
+		if( $result )
+		{
+			$num_rows = $result->num_rows;
+			$field_count = $result->field_count;
+			$fields = $result->fetch_fields(); //returns an array
+			echo "<table>";
+			echo "<tr>";
+			for($i = 0; $i < $field_count; $i++)
+			{
+				echo "<th>";
+				echo $fields[$i]->name;
+				echo "</th>";
+			}
+			echo "</tr>";
+			while( $row = $result->fetch_row() )
+			{
+				echo "<tr>";
+				for( $i=0; $i<$field_count; $i++ )
+				{
+					echo "<td>";
+					echo "$row[$i]";
+					echo "</td>";
+				}
+			}
+			echo "</table>";
+		}
+		else
+		{
+			echo "QUERY FAILED: ".$mysqli->error;
+		}
+		$mysqli->free();
+		$mysqli->close();
+	}
+?>
+
+</body>
+</html>
